0fd0ec05e1bef9e3e2a268fa7ef4fa0aee541684
Chris Shellenberger <cshellen1@gmail.com>
2023-05-23 16:55:43 -0600

    adding session memory to clear stored responses

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY = 'responses'
 QUESTIONS = satisfaction_survey.questions
 
 @app.route('/')
@@ -17,35 +17,43 @@ def show_home_page():
     """get home page and button to start survey"""
     return render_template('home.html', survey=satisfaction_survey)
 
-@app.route('/start')
+@app.route('/start', methods=['POST'])
 def start_survey():
     """reset the responses and handle starting the survey"""
     
-    responses = []
+    session[RESPONSES_KEY] = []
+    
     return redirect('/question/0')
 
 @app.route('/question/<int:qid>')
 def get_question(qid):
     """get question form survey and return html for question"""
-    if (qid is not len(responses)):
-        flash('Invalid question ID')
-        return redirect(f'/question/{len(responses)}')
     
+    responses = session.get(RESPONSES_KEY)
+    
+    if ( responses is None):
+        return redirect('/')
+        
         
     if (qid == len(QUESTIONS)):
         return redirect('/finish')
     
-    else:
-        question = QUESTIONS[qid].question 
-        choices = QUESTIONS[qid].choices
-        return render_template('question.html', question=question, choices=choices, )
+    if (qid != len(responses)):
+        flash('Invalid question ID')
+        return redirect(f'/question/{len(responses)}')
+    
+    question = QUESTIONS[qid].question 
+    choices = QUESTIONS[qid].choices
+    return render_template('question.html', question=question, choices=choices, )
 
 
 @app.route('/answer', methods=['POST'])
 def handle_question_answer():
     """handle submitted response and update stored responses"""
     response = request.form['answer']
+    responses = session[RESPONSES_KEY]
     responses.append(response)
+    session[RESPONSES_KEY] = responses
     
     if len(responses) == len(QUESTIONS):
         return redirect ('/finish')
